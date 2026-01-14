097f215e5734f1c1c2d3075ccfa5c0b3392e2cfb
Andry Ariadi <andryariadi23@gmail.com>
2026-01-14 14:51:54 +0800

    feat(ui): complete parent UI design

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,27 @@
+import Announcements from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
+import EvCalendar from "@/components/EvCalendar";
+
 const ParentPage = () => {
-  return <div>ParentPage</div>;
+  return (
+    <section className="flex flex-col md:flex-row gap-4">
+      {/* Left - Calendar */}
+      <div className="w-full lg:max-w-2/3 rounded-xl">
+        <div className="bg-white p-4 rounded-md space-y-3">
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* Right - Events & Announcements */}
+      <div className="w-full lg:max-w-1/3 rounded-xl space-y-5">
+        <EvCalendar />
+
+        <Announcements />
+      </div>
+    </section>
+  );
 };
 
 export default ParentPage;
